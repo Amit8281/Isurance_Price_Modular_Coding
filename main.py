d86daa6a779b655ce161da35fccf5d2c4671ba03
Amit8281 <ak15072419@gmail.com>
2023-08-16 00:48:47 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys, os
 from Insurance.utils import get_collection_as_dataframe
 from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
+from Insurance.components.data_ingestion import DataIngestion
 
 
 
@@ -24,11 +25,15 @@ if __name__=="__main__":
          #get_collection_as_dataframe(database_name="INSURANCE",collection_name="INSURANCE_PROJECT")
         training_pipeline_config = config_entity.TrainingPipelineConfig()
        
-      #data ingestion
+       #data ingestion
         data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         print(data_ingestion_config.to_dict())
-        #data_ingestion = DataIngestionConfig(data_ingestion_config=data_ingestion_config)
-        #data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+        
+     
+     
      except Exception as e:
          print(e)
          
